eb077b1799f89f3b7409044639023b9e8c97fc24
cjpalm <curtis.palm@qiagen.com>
2014-05-13 14:04:48 -0700

    comitting run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,6 @@
+train.data.dt <- read.table("./UCI HAR Dataset/train/X_train.txt");
+test.data.dt <- read.table("./UCI HAR Dataset/test/X_test.txt");
+test.labels.dt <- read.table("./UCI HAR Dataset/test/y_test.txt");
+train.labels.dt <- read.table("./UCI HAR Dataset/train/y_train.txt");
+train.subject.dt <- read.table("./UCI HAR Dataset/train/subject_train.txt");
+test.subject.dt <- read.table("./UCI HAR Dataset/test/subject_test.txt");
