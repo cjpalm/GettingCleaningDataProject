853f21817a828f16bc40bbd1bb661015b9df9662
cjpalm <curtis.palm@qiagen.com>
2014-05-25 12:49:13 -0700

    added codebook details, variables added write final table

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 ##Getting Cleaning Data Course Project
-The script "run_analysis.R" does the tasks listed in course project assignmemt. These tasks are:
+The script "run_analysis.R" does the tasks listed in course project assignment. These tasks are:
   1. Merges the training and the test sets to create one data set.
   2. Extracts only the measurements on the mean and standard deviation for each measurement. 
   3. Uses descriptive activity names to name the activities in the data set
   4. Appropriately labels the data set with descriptive activity names. 
   5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
-the "run_analyis.R" script contains all the code needed to preform these tasks.
+the "run_analysis.R" script contains all the code needed to preform these tasks.
 
 
 ##  Steps taken to produce the tidy data set.
 
-Details are in the CodeBook.md and comnents in the "run_analysis.R"  Script.  All steps needed to process the data and produce the tidy dataset are done by the "run_analysis.R" script.  If the dataset is not already present in the working directory, internet access is required to download the data.
+Details are in the CodeBook.md and comments in the "run_analysis.R"  Script.  All steps needed to process the data and produce the tidy dataset are done by the "run_analysis.R" script.  If the data set is not already present in the working directory, internet access is required to download the data.
--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,28 @@
-## Load data. assume it is in subdir of working dir.
+## check to see if dataset is in working dir. if not download it and unzip it.
+if(!file.exists("./UCI HAR Dataset")){
+  url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" 
+  download.file(url,destfile="./courseProject.zip",method="curl") 
+  unzip("./courseProject.zip")
+}
+
+## Load data.
 train.data.dt <- read.table("./UCI HAR Dataset/train/X_train.txt");
 test.data.dt <- read.table("./UCI HAR Dataset/test/X_test.txt");
+
 ## load activity labels.
 test.labels.dt <- read.table("./UCI HAR Dataset/test/y_test.txt");
 train.labels.dt <- read.table("./UCI HAR Dataset/train/y_train.txt");
+
 ##load subject IDs
 train.subject.dt <- read.table("./UCI HAR Dataset/train/subject_train.txt");
 test.subject.dt <- read.table("./UCI HAR Dataset/test/subject_test.txt");
+
 ## load names of measurements
 features.dt <- read.table("./UCI HAR Dataset/features.txt");
+
 ##  vector of activities that correspond to labels
 activities <- c("walking","walking.upstairs","walking.downstairs","sitting","standing","laying")
+
 ## clean up feature names
 features.dt[[2]] <- tolower(features.dt[[2]]);
 features.dt[[2]]  <- gsub("\\(\\)","",features.dt[[2]])
@@ -18,27 +30,31 @@ features.dt[[2]]  <- gsub("\\)","",features.dt[[2]])
 features.dt[[2]]  <- gsub("\\(",".",features.dt[[2]])
 features.dt[[2]]  <- gsub(",",".",features.dt[[2]])
 features.dt[[2]]  <-  gsub("-",".",features.dt[[2]])
+
 ## merge train and test data,labels and subjects
 all.data.dt <- rbind(train.data.dt,test.data.dt)
 all.labels.dt <- rbind(train.labels.dt,test.labels.dt)
 all.text.labels.dt <- activities[all.labels.dt[[1]]]
 all.subject.dt <- rbind(train.subject.dt,test.subject.dt)
+
 ## column names to data
 colnames(all.data.dt) <-c(features.dt[[2]])
+
 ## subset all mean features - "exclude meanfreq" features
 mean.features.dt <- features.dt[grep("mean",features.dt[[2]]) ,]
 mean.features.dt <- mean.features.dt[grep("freq",mean.features.dt[[2]],invert=TRUE),]
+
 ## subest all std.dev. features.
 std.features.dt <- features.dt[grep("std",features.dt[[2]]) ,]
+
 ## combine mean and std.dev. and sort them
 mean.std.features.dt <- rbind(mean.features.dt,std.features.dt) 
 columns.to.keep <- sort(mean.std.features.dt[[1]])
+
 ## extract columns to keep
 mean.std.data.dt <- all.data.dt[,columns.to.keep]
-##
 
-
-# create data frame 
+## create data frame 
 mean.std.data.df <- data.frame(mean.std.data.dt,all.text.labels.dt,all.subject.dt)
 
 ## add proper column headers
@@ -49,6 +65,13 @@ colnames(mean.std.data.df) <- newcolnames
 
 #split the data 
 s <- split(mean.std.data.df,list(mean.std.data.df$activity,mean.std.data.df$subject), drop=TRUE )
+
 # calculate the means
-finaltable <- lapply(s,function(x) colMeans(x[,newcolnames[1:73]]))
+means.list <- lapply(s,function(x) colMeans(x[,newcolnames[1:73]]))
+
+#convert to dataframe
+tidy.table.dt  <- do.call(cbind.data.frame, means.list)
+
+# save the dataframe to disk
+write.csv(tidy.table.dt,file="./courseProjectTidyTable.csv")
 
